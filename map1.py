bca8e4fa5f0744b122448caab1d77f23b544cbcc
KostiganSavin <kostigan.savin@gmail.com>
2017-09-12 22:37:25 +0300

    Add Layer with poppulation. Split Volcanoes and Poppulation in two layers. Add LayerConmtrol

--- map1.py
+++ map1.py
@@ -16,18 +16,23 @@ def color_producer(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup("My Map")
+fgv = folium.FeatureGroup("Volcanoes")
 for lt, ln, el in zip(lat, lon, elev):
     p = folium.Popup(str(el)+" m", parse_html=True)
-    fg.add_child(folium.CircleMarker(
-                                              location=[lt, ln],
-                                              popup=p,
-                                              radius=5,
-                                              color="grey",
-                                              fill_color=color_producer(el),
-                                              fill_opacity=0.7,
-                                              fill=True))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=p,
+                                     radius=5, color="grey", 
+                                     fill_color=color_producer(el),
+                                     fill_opacity=0.7, fill=True))
     # fg.add_child(folium.Marker(location=[lt, ln], popup=p, icon=folium.Icon(color=color_producer(el))))
-map.add_child(fg)
+
+fgp = folium.FeatureGroup("Poppulation")
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+                 style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 
+                 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
